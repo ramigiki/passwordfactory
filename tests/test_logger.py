06b476766c8bc21b5a2c5188d08f5f77b23999ae
Rameez Hussain <hussain.gik294@gmail.com>
2022-08-08 23:25:31 +0500

    import error fix: logger test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,7 +1,7 @@
-from app.logger import Logger
+from app.logger import get_logger
 
 
 def test_logger_singelton():
-    logger_1 = Logger().get_logger()
-    logger_2 = Logger().get_logger()
+    logger_1 = get_logger()
+    logger_2 = get_logger()
     assert logger_1 == logger_2
